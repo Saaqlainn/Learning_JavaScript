4236ecbc8cd20015ce13f3404e4bc598b7eca885
Saqlain <Saqlainpatel727@gmail.com>
2021-11-09 13:25:55 +0000

    Console logs, Errors, Warnings and More

---
+++ Notes-02.js
@@ -0,0 +1,19 @@
+// console is a very powerful tool. in this
+// tutorial we gonna see how to lock working , numbers , arrays and much more 
+
+console.time('your code took');/* this is to check the time taken to run code */
+console.log('hello console');
+console.log(4 + 43); /* this is an intiger */
+console.log(34); /* this is an intiger */
+console.log(true); /* this is an boolean */
+console.log([34, 3, 5, 6]); /* this is an array */
+console.log({ saqlain: 'Javascript', marks: 40 }); /* this is an javscript Object */
+console.table({ saqlain: 'Javascript', marks: 40 }); /* this is an js Object in tabular form*/
+console.warn('this is a warning'); /* this is an warning */
+console.timeEnd('your code took');/* time end show that the ------- */
+// console.assert(age<189, 'age > is not possible')
+console.error('this is an error');
+// console.clear() /*  code is use to clear console */
+
+/* A best practice is use a semicolon after you finished your sentense */
+/* but the javascript is forgiving language it doesnt shows an error when there is no semicolon it still runs the code */
